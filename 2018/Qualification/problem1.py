b035078444d48327f32f0c848fd7f3ea4f55a112
Eugene Yarmash <e.yarmash@gmail.com>
2018-04-08 22:46:53 +0300

    Presumably fix the issue with the second test set
    Always swap the *last* 'C' instruction because that's the one that saves
    us the most damage.
    
    Also, do some cleanup.

--- 2018/Qualification/problem1.py
+++ 2018/Qualification/problem1.py
@@ -1,3 +1,4 @@
+
 def calc_damage(s):
     damage = 0
     strength = 1
@@ -12,30 +13,34 @@ def calc_damage(s):
 
 
 def swap_one(L):
-    for i in range(len(L)-1):
+    for i in range(len(L)-2, -1, -1):
         if L[i] == 'C' and L[i+1] == 'S':
             L[i], L[i+1] = L[i+1], L[i]
             return True
     return False
 
+# TODO: need to verify the solution after modifying
+
 
 def calc_hacks(D, P):
     d = calc_damage(P)
+    if d == 0:
+        return 0
+
     h = 0
 
     if d > D:
         if 'C' not in P:
-            h = 'IMPOSSIBLE'
-        else:
-            L = list(P)
-            while swap_one(L):
-                h += 1
-                d = calc_damage(L)
-                if d <= D:
-                    break
+            return 'IMPOSSIBLE'
 
-        if d > D:
-            h = 'IMPOSSIBLE'
+        L = list(P)
+        while swap_one(L):
+            h += 1
+            d = calc_damage(L)
+            if d <= D:
+                return h
+
+        return 'IMPOSSIBLE'
 
     return h
 
