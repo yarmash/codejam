cc5aa030b3c5876e66fb5b4ac21737cefce994ee
Eugene Yarmash <e.yarmash@gmail.com>
2018-04-09 23:25:18 +0300

    Add a solution to the problem #3 (Go, Gopher!)
    Link to the problem description:
    https://codejam.withgoogle.com/2018/challenges/00000000000000cb/dashboard/0000000000007a30

---
+++ 2018/Qualification/problem3.py
@@ -0,0 +1,30 @@
+
+def free_cells(M, i, j):
+    return sum(1 for c in (M[i][j], M[i-1][j-1], M[i-1][j], M[i-1][j+1], M[i][j+1], M[i+1][j+1], M[i+1][j], M[i+1][j-1], M[i][j-1]) if not c)
+
+
+def main():
+    import sys
+
+    T = int(input())
+    for i in range(T):
+        A = int(input())
+
+        M = [[0]*1000 for _ in range(1000)]
+
+        candidates = [(x, y) for x in range(500+1, 500+4-1) for y in range(500+1, 500+A//4-1)]
+
+        I = J = None
+
+        while not (I == J == 0):
+            c = max(candidates, key=lambda c: free_cells(M, c[0], c[1]))
+            print(c[0], c[1])
+            sys.stdout.flush()
+            I, J = map(int, input().split())
+            if I == J == -1:
+                sys.exit()
+
+            M[I][J] = 1
+
+
+main()
