abde54161d11b9d395a014f5fd88a0f0ee2171f8
Eugene Yarmash <e.yarmash@gmail.com>
2018-04-15 16:05:21 +0300

    Split chunks to subchunks, check if there's enough dice

--- 2017/Finals/problemA.py
+++ 2017/Finals/problemA.py
@@ -1,14 +1,38 @@
 from operator import itemgetter
-from itertools import groupby
+from itertools import groupby, chain
 
 
 def get_straight(chunk: list, idx: int, straight: int, dice: set) -> int:
+    """Get max straight starting at index idx."""
     if idx == len(chunk) - 1:
         return straight
 
     item = chunk[idx]
 
     return max((get_straight(chunk, idx+1, straight+1, dice-{x}) for x in item[1] if x in dice), default=straight)
+
+
+def check_dice(chunk):
+    """Check that chunk has enough unique dice to arrange in a row."""
+
+    # number of unique dice should be greater than or equal to the length of the chunk
+    for i in range(len(chunk), 1, -1):
+        for j in range(0, len(chunk)-i+1, i):
+            subchunk = chunk[j:j+i]
+
+            if len(set(chain.from_iterable(map(itemgetter(1), subchunk)))) < len(subchunk):
+                return False
+
+    return True
+
+
+def get_subchunks(chunk):
+    """Generate subchunks from biggest to smallest."""
+    yield chunk
+
+    for size in range(len(chunk) - 1, 1, -1):
+        for i in range(len(chunk) - size + 1):
+            yield chunk[i:i+size]
 
 
 def main():
@@ -37,15 +61,27 @@ def main():
         max_straight = 1
 
         for chunk in chunks:
+            #print("CHUNK: ", chunk)
+
             if len(chunk) <= max_straight:
+                #print("BREAK (too small chunk)")
                 break
 
-            for k in range(len(chunk)-1):
-                if len(chunk) - k < max_straight:
+            for subchunk in get_subchunks(chunk):
+                #print("SUBCHUNK: ", subchunk)
+
+                if len(subchunk) <= max_straight:
+                    #print("BREAK (too small subchunk)")
                     break
+
+                if not check_dice(subchunk):
+                    #print("CONTINUE (not enough dice)", subchunk)
+                    continue
+
                 dice = set(range(ndice))
-                straight = get_straight(chunk, k, 0, dice)
+                straight = get_straight(subchunk, 0, 1, dice)
                 if straight > max_straight:
+                    #print("NEW STRAIGHT: ", straight)
                     max_straight = straight
 
         print("Case #{}: {}".format(i+1, max_straight))
