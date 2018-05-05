be7863ff88bb7ffa09eec3c64e9e26064b52be38
Eugene Yarmash <e.yarmash@gmail.com>
2018-05-05 15:47:19 +0300

    Optimize the solution
    Inspired by https://github.com/salvois/codejam/blob/master/src/codejam2017/finals/DiceStraight.java
    
    The basic idea of the algorithm is to put all values on the faces of all dice in a list
    sorted by value, storing the set of containing dice with each value. We then scan values
    until consecutive values are present, and until we can  use a different die for each value.
    When no free die is found for a value, we try to recursively shuffle the dice already used
    for other values so that we can free a die for the new value.

--- 2017/Finals/problemA.py
+++ 2017/Finals/problemA.py
@@ -1,43 +1,139 @@
 #!/usr/bin/env python
 
-from collections import defaultdict
-from itertools import groupby
+import sys
+from itertools import starmap
+
+# TODO: get rid of recursion
+sys.setrecursionlimit(30000)
 
 
-def straights(chunk, idx, free_dice):
-    """Generate straights for a chunk starting at index idx"""
+class Die:
+    def __init__(self, id=None, current_value=None):
+        self.id = id
+        self.current_value = current_value
 
-    if idx == len(chunk) - 1:
-        yield idx + 1
-    else:
-        for x in chunk[idx]:
-            if x in free_dice:
-                yield from straights(chunk, idx+1, free_dice.difference((x,)))
-            else:
-                yield idx + 1
+    def __repr__(self):
+        return f'Die({self.id!r})'
 
 
-def check_dice(chunk):
-    """Check that chunk has enough unique dice to form a solution."""
+class DieValue:
+    def __init__(self, value, dice):
+        self.value = value
+        self.dice = dice  # Set of dice containing this value
 
-    # number of unique dice should be greater than or equal to the length of the chunk
-    for i in range(len(chunk), 1, -1):
-        for j in range(0, len(chunk)-i+1, i):
-            subchunk = chunk[j:j+i]
+    def __repr__(self):
+        return f'DieValue({self.value!r}, {self.dice!r})'
 
-            if len(set().union(*subchunk)) < len(subchunk):
-                return False
+    def find_unused_die(self):
+        """Returns the first unused die among the ones containing this value, or None if not found."""
+        return next((die for die in self.dice if die.current_value is None), None)
 
-    return True
+    def assign_die(self):
+        """If no die is used by this value, use the first die among the ones containing this value."""
+        if all(die.current_value is None for die in self.dice):
+            self.dice[0].current_value = self
+
+    def unassign_die(self):
+        """If a die is used by this value, unassign it."""
+        for die in self.dice:
+            if die.current_value == self:
+                die.current_value = None
+                break
 
 
-def get_subchunks(chunk):
-    """Generate subchunks from biggest to smallest."""
-    yield chunk
+class TestCase:
+    visited = set()  # To skip already visited dice when recursively shuffling
 
-    for size in range(len(chunk) - 1, 1, -1):
-        for i in range(len(chunk) - size + 1):
-            yield chunk[i:i+size]
+    def __init__(self, ndice, values):
+        self.ndice = ndice
+        self.values = values
+
+    def free_by_shuffling(self, die):
+        """
+        Attempt to recursively free a die by selecting a different die for the same value.
+        Return True if the die has been freed, False if no other die can be found.
+        """
+        assert die.current_value is not None
+
+        # First check if we can just use another dice for the previous value
+        for other_die in die.current_value.dice:
+            if other_die.current_value is None:
+                other_die.current_value = die.current_value
+                die.current_value = None
+                return True
+
+        # Nope, we must free a die recursively
+        self.visited.add(die)
+
+        for other_die in die.current_value.dice:
+            if other_die in self.visited:
+                continue
+            if self.free_by_shuffling(other_die):
+                other_die.current_value = die.current_value
+                die.current_value = None
+                return True
+        return False
+
+    def find_unused_by_shuffling(self, value):
+        """
+        Attempt to find an unused die for the specified value by possibly shuffling other already used dice.
+        Return a now-unused die, or None if no shuffling results in an unused die.
+        """
+
+        self.visited.clear()
+
+        for die in value.dice:
+            if self.free_by_shuffling(die):
+                return die
+
+        return None
+
+    def find_longest_straight(self):
+        """Finds the maximum length of dice with consecutive values."""
+        max_length = 1
+        end_index = 0
+
+        for begin_index, value in enumerate(self.values):
+            value.assign_die()
+
+            begin_value = value.value
+            max_possible_length = min(len(self.values) - begin_index, self.ndice)
+
+            next_begin_index = begin_index + 1
+
+            if end_index <= begin_index:
+                end_index = begin_index + 1
+
+            while end_index < len(self.values):
+                value = self.values[end_index]
+                if value.value != begin_value + end_index - begin_index:
+                    # Not a consecutive value, restart scanning from there
+                    next_begin_index = end_index
+                    break
+
+                die = value.find_unused_die()
+                if die is None:
+                    die = self.find_unused_by_shuffling(value)
+                if die is None:
+                    break
+
+                die.current_value = value
+                end_index += 1
+
+                if end_index - begin_index > max_length:
+                    max_length = end_index - begin_index
+
+                if max_length == max_possible_length:
+                    break
+
+            if max_length == max_possible_length:
+                break
+
+            # The old values at the beginning are no longer useful, free their dice
+            for idx in range(begin_index, next_begin_index):
+                self.values[idx].unassign_die()
+
+        return max_length
 
 
 def main():
@@ -45,52 +141,19 @@ def main():
 
     for i in range(T):
         ndice = int(input())
-        integers = defaultdict(list)
+        value2dice = {}  # map values to lists of dice
 
-        for j in range(ndice):
-            for integer in map(int, input().split()):
-                integers[integer].append(j)
+        for j in range(1, ndice+1):
+            die = Die(id=j)
 
-        #print(integers)
-        chunks = []
+            for value in map(int, input().split()):
+                value2dice.setdefault(value, []).append(die)
 
-        # find runs of consecutive numbers
-        for k, g in groupby(enumerate(sorted(integers)), lambda x: x[0]-x[1]):
-            g = list(g)
-            if len(g) > 1:
-                chunks.append([integers[x[1]] for x in g])
+        # Set of distinct values on the face of all dice, sorted by value
+        values = list(starmap(DieValue, sorted(value2dice.items())))
+        case = TestCase(ndice, values)
 
-        chunks.sort(key=len, reverse=True)
+        print("Case #{}: {}".format(i+1, case.find_longest_straight()))
 
-        max_straight = 1
-
-        for chunk in chunks:
-            #print(f"CHUNK of length {len(chunk)}: ", chunk)
-
-            if len(chunk) <= max_straight:
-                #print("BREAK (too small chunk)")
-                break
-
-            for subchunk in get_subchunks(chunk):
-                #print(f"-> SUBCHUNK of length {len(subchunk)}: ", subchunk)
-
-                if len(subchunk) <= max_straight:
-                    #print("--> BREAK (too small subchunk)")
-                    break
-
-                if not check_dice(subchunk):
-                    #print("--> CONTINUE (not enough dice)", subchunk)
-                    continue
-
-
-                for straight in straights(subchunk, 0, set(range(ndice))):
-                    #print(straight)
-                    if straight > max_straight:
-                        #print("--> NEW MAX STRAIGHT: ", straight)
-                        max_straight = straight
-                        if straight == len(subchunk):
-                            break
-
-        print("Case #{}: {}".format(i+1, max_straight))
 
 main()
