6d39c7c1f3919b2fe5df19d341876fab88aefc27
Eugene Yarmash <e.yarmash@gmail.com>
2018-08-05 18:00:14 +0300

    Optimize the solution
    Some ideas:
    
    We want the fractional part of the percentage of each programming language to
    be greater than or equal to 0.5, so that it rounds up during the final sum.
    Thus, whenever we find a fractional part less than 0.5 we add the percentage
    of one of the remaining people to try to make the fractional at least 0.5.
    We give priority to percentages whose fractional part is as close as possible
    to 0.5, in order to maximize the number of percentages we can bring above
    the 0.5 threshold.
    If we have no percentages whose fractional part is less than 0.5, we
    add a new percentage for a new programming language, and repeat the process.
    We don't touch percentages whose fractional part is already >= 0.5

--- 2018/1B/problem1.py
+++ 2018/1B/problem1.py
@@ -1,39 +1,46 @@
-from functools import reduce
+#!/usr/bin/env python
+
+"""Rounding Error"""
+
+from heapq import heappop, heappush
 
 
 def main():
-    def gen_sums(total, freq, remaining):
-        #print(total, freq, remaining)
-        if not remaining:
-            #print(total, freq)
-            yield reduce(lambda x, y: x + (y*100+total//2)//total, freq, 0)
-        else:
-            seen = set()
+    T = int(input())  # the number of test cases
 
-            for i in range(len(freq)):
-                if not freq[i] in seen:
-                    yield from gen_sums(total, freq[:i] + [freq[i]+1] + freq[i+1:], remaining-1)
-                    seen.add(freq[i])
-
-            yield from gen_sums(total, freq+[1], remaining-1)
-
-    T = int(input())
-
-    for i in range(1, T+1):
+    for case in range(1, T+1):
         total_people, num_languages = map(int, input().split())
-        languages_frequency = [int(x) for x in input().split()]
+        freq = map(int, input().split())
 
-        if not 100 % total_people:
-            print('Case #{}: {}'.format(i, 100))
-            continue
+        percent_per_person = 100/total_people
+        low_scores = []
+        not_responded = total_people
+        res = 0
 
-        #assert(len(languages_frequency) == num_languages)
-        not_responded = total_people - sum(languages_frequency)
+        for f in freq:
+            p = f*percent_per_person
+            if 0 < p-int(p) < 0.5:
+                heappush(low_scores, (-(p-int(p)), p))
+            else:
+                res += int(p+0.5)  # Python3's round() won't work here!
+            not_responded -= f
 
+        while not_responded:
+            try:
+                _, p = heappop(low_scores)
+            except IndexError:
+                p = 0
 
-        #print(total_people, num_languages, not_responded, languages_frequency)
-        max_percentage = max(gen_sums(total_people, languages_frequency, not_responded))
+            p += percent_per_person
 
-        print('Case #{}: {}'.format(i, max_percentage))
+            if 0 < p-int(p) < 0.5:
+                heappush(low_scores, (-(p-int(p)), p))
+            else:
+                res += int(p+0.5)
+            not_responded -= 1
+
+        res += sum(int(x[1]+0.5) for x in low_scores)
+
+        print('Case #{}: {}'.format(case, res))
 
 main()
