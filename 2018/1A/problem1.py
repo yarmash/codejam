c0de2d173d98f297872b95e2409c8a0f3b59e48e
Eugene Yarmash <e.yarmash@gmail.com>
2018-08-09 14:41:59 +0300

    Optimize the solution
    Use the ideas from the analysis

--- 2018/1A/problem1.py
+++ 2018/1A/problem1.py
@@ -2,79 +2,86 @@
 
 """Waffle Choppers"""
 
-from itertools import chain
+
+def calc_cumulative_sums(W, R, C):
+    """Calculate cumulative sums for rows and columns of the waffle"""
+
+    S = 0  # total chips
+    SR = [0]*R
+    SC = [0]*C
+
+    for r in range(R):
+        for c in range(C):
+            has_chip = W[r][c] == '@'
+            S += has_chip
+            SR[r] += has_chip
+            SC[c] += has_chip
+
+    for i in range(1, len(SR)):
+        SR[i] += SR[i-1]
+
+    for i in range(1, len(SC)):
+        SC[i] += SC[i-1]
+
+    return SR, SC, S
 
 
 def check(R, C, H, V, W):
-    total_chips = sum(W[r][c] == '@' for r in range(R) for c in range(C))
-    pieces = (H + 1) * (V + 1)
+    SR, SC, S = calc_cumulative_sums(W, R, C)
+
+    pieces = (H + 1)*(V + 1)
 
     # the total number of chips must be divisible by the number of pieces
-    if total_chips % pieces:
+    if S % pieces:
         return 'IMPOSSIBLE'
 
-    chips_per_piece = total_chips // pieces
+    chips_per_hslice = S // (H + 1)
+    chips_per_vslice = S // (V + 1)
 
-    # each vertical slice must contain chips for H+1 pieces
-    vcut_chips = (H + 1)*chips_per_piece
+    # each of the V + 1 vertical slices must have exactly S/(V + 1) chips
     vcuts = []
-    prev_vcut = 0
-
-    for vcut in range(C):
-        nchips = 0
-
-        for row in range(R):
-            for col in range(prev_vcut, vcut+1):
-                nchips += W[row][col] == '@'
-
-        if nchips == vcut_chips:
-            vcuts.append(vcut)
-
+    next_sum = chips_per_vslice
+    for col, col_sum in enumerate(SC):
+        if col_sum == next_sum:
+            vcuts.append(col)
             if len(vcuts) == V:
-                if total_chips - vcut_chips * len(vcuts) != vcut_chips:
+                if SC[-1] - next_sum != chips_per_vslice:
                     return 'IMPOSSIBLE'
                 break
-            prev_vcut = vcut+1
+            next_sum += chips_per_vslice
     else:
         return 'IMPOSSIBLE'
 
-    # each horizontal slice must contain chips for V+1 pieces
-    hcut_chips = (V + 1)*chips_per_piece
+    # each of the H + 1 horizontal slices must have exactly S/(H + 1) chips
     hcuts = []
-    prev_hcut = 0
-
-    for hcut in range(R):
-        nchips = 0
-        for col in range(C):
-            for row in range(prev_hcut, hcut+1):
-                nchips += W[row][col] == '@'
-
-        if nchips == hcut_chips:
-            hcuts.append(hcut)
+    next_sum = chips_per_hslice
+    for row, row_sum in enumerate(SR):
+        if row_sum == next_sum:
+            hcuts.append(row)
             if len(hcuts) == H:
-                if total_chips - hcut_chips * len(hcuts) != hcut_chips:
+                if SR[-1] - next_sum != chips_per_hslice:
                     return 'IMPOSSIBLE'
                 break
-            prev_hcut = hcut+1
+            next_sum += chips_per_hslice
     else:
         return 'IMPOSSIBLE'
 
     # each resulting rectangle must contain exactly the number of chips per piece
-    prev_nchips = None
-    col = 0
-    for vcut in chain(vcuts, [C-1]):  # add explicit cuts at the ends of the grid
-        row = 0
-        for hcut in chain(hcuts, [R-1]):
-            nchips = 0
-            for r in range(row, hcut+1):
-                for c in range(col, vcut+1):
-                    if W[r][c] == '@':
-                        nchips += 1
-            if prev_nchips is not None and nchips != prev_nchips:
+    chips_per_piece = S // pieces
+    # add explicit cuts after the last row/column
+    hcuts.append(R-1)
+    vcuts.append(C-1)
+
+    prev_col = 0
+    for vcut in vcuts:
+        prev_row = 0
+        for hcut in hcuts:
+            nchips = sum(W[r][c] == '@' for r in range(prev_row, hcut+1)
+                         for c in range(prev_col, vcut+1))
+            if nchips != chips_per_piece:
                 return 'IMPOSSIBLE'
-            prev_nchips = nchips
-            row = hcut+1
-        col = vcut+1
+            prev_row = hcut + 1
+        prev_col = vcut + 1
 
     return 'POSSIBLE'
 
