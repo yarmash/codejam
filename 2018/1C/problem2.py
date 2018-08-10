ab897b0f795a5b05cbcc7371215994f00d791113
Eugene Yarmash <e.yarmash@gmail.com>
2018-08-10 12:20:43 +0300

    Add a solution to the problem #2 (Lollipop Shop)

---
+++ 2018/1C/problem2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Lollipop Shop"""
+
+
+def main():
+    T = int(input())  # the number of test cases
+
+    for case in range(1, T+1):
+        N = int(input())  # the number of lollipops/customers
+        F = set(range(N))  # available flavors
+
+        freq = [0]*N
+
+        for _ in range(N):
+            D, *P = map(int, input().split())  # number of flavors, preferences
+
+            if D == 0:
+                print(-1, flush=True)
+                continue
+
+            for p in P:
+                freq[p] += 1
+
+            for x in sorted(P, key=freq.__getitem__):
+                if x in F:
+                    print(x, flush=True)
+                    F.remove(x)
+                    break
+            else:
+                print(-1, flush=True)
+
+main()
