15e1d6c200c314bdee0467cde88f4f2139d4b6a3
Eugene Yarmash <e.yarmash@gmail.com>
2018-08-15 13:09:53 +0300

    Optimize the solution
    Find all the ways to partition bits among the cashiers.
    This solves the 1st test set, but produces 'Runtime error' for the 2nd.

--- 2018/1A/problem2.py
+++ 2018/1A/problem2.py
@@ -2,21 +2,16 @@
 
 """Bit Party"""
 
+from itertools import chain, combinations, combinations_with_replacement
 
-def search(avail_cashiers, chosen_cashiers, B, R):
-    if B == 0:
-        yield max(k[1]*v + k[2] for k, v in chosen_cashiers.items())
-    else:
-        if R > 0:
-            for c in avail_cashiers:
-                bits = min(B, c[0])
 
-                for b in range(1, bits+1):
-                    avail = [cashier for cashier in avail_cashiers if cashier != c]
-                    chosen = chosen_cashiers.copy()
-                    chosen[c] = b
+class Cashier:
+    __slots__ = ('max_bits', 'scan_time', 'packaging_time')
 
-                    yield from search(avail, chosen, B-b, R-1)
+    def __init__(self, M, S, P):
+        self.max_bits = M
+        self.scan_time = S
+        self.packaging_time = P
 
 
 def main():
@@ -29,11 +24,29 @@ def main():
 
         for _ in range(C):
             M, S, P = map(int, input().split())  # max number of bits, scan time per bit, packaging time
-            cashiers.append((M, S, P))
+            cashier = Cashier(M, S, P)
+            cashiers.append(cashier)
 
-        time = min(search(cashiers, {}, B, R))
+        res = float('inf')
 
-        print('Case #{}: {}'.format(case, time))
+        for num_cashiers in range(1, R+1):
+            for sel_cashiers in combinations(cashiers, num_cashiers):
+                for bits_bars in combinations_with_replacement(range(B+1), num_cashiers-1):
+                    prev_bar = 0
+                    times = []
+
+                    for cashier, bar in zip(sel_cashiers, chain(bits_bars, (B,))):
+                        num_bits = bar - prev_bar
+                        prev_bar = bar
+                        if num_bits > cashier.max_bits:
+                            break
+                        times.append(num_bits*cashier.scan_time + cashier.packaging_time)
+                    else:
+                        time = max(times)
+                        if time < res:
+                            res = time
+
+        print('Case #{}: {}'.format(case, res))
 
 
 main()
