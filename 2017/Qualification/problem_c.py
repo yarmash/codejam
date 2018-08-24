513b9f111aac153c73a6aa1b4f9eea5e7db60d20
Eugene Yarmash <e.yarmash@gmail.com>
2018-08-24 14:34:18 +0300

    Optimize the solution
    Copy the solution from the 2018's Practice Session

--- 2017/Qualification/problem_c.py
+++ 2017/Qualification/problem_c.py
@@ -3,45 +3,39 @@
 """Bathroom Stalls"""
 
 from collections import defaultdict
-from heapq import heappop, heappush
 
 
 def main():
     T = int(input())  # the number of test cases
 
     for case in range(1, T+1):
+
         N, K = map(int, input().split())
-        h = [-N]
-        cnt = defaultdict(int)
-        cnt[N] += 1
+        S = {N}
+        count = defaultdict(int)
+        count[N] = 1
+        P = 0
 
-        for _ in range(K):
-            chunk = -h[0]
-            cnt[chunk] -= 1
-
-            if not cnt[chunk]:
-                heappop(h)
+        while True:
+            chunk = max(S)
 
             if chunk & 1:
-                if chunk == 1:
-                    min_s = max_s = 0
-                else:
-                    min_s = max_s = chunk // 2
-                    if not cnt[min_s]:
-                        heappush(h, -min_s)
-
-                    cnt[min_s] += 2
+                min_s = max_s = chunk // 2
             else:
                 max_s = chunk // 2
                 min_s = max_s - 1
 
-                if min_s and not cnt[min_s]:
-                    heappush(h, -min_s)
-                if not cnt[max_s]:
-                    heappush(h, -max_s)
+            P += count[chunk]
 
-                cnt[min_s] += 1
-                cnt[max_s] += 1
+            if P >= K:
+                break
+
+            S.remove(chunk)
+            S.add(min_s)
+            S.add(max_s)
+
+            count[max_s] += count[chunk]
+            count[min_s] += count[chunk]
 
         print('Case #{}: {} {}'.format(case, max_s, min_s))
 
