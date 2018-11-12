4d2981f0a306b874c3768e41a3db90c7bf1105ea
Eugene Yarmash <e.yarmash@gmail.com>
2018-11-12 15:09:38 +0300

    Solve the large input
    Use the ideas from the analysis

--- 2016/Qualification/problem_d.py
+++ 2016/Qualification/problem_d.py
@@ -9,7 +9,18 @@ def main():
     for case in range(1, T+1):
         K, C, S = map(int, input().split())
 
-        print(f'Case #{case}: ', *range(1, K+1))
+        if C*S < K:
+            print(f'Case #{case}: IMPOSSIBLE')
+            continue
+
+        tiles = []
+        for i in range(1, K + 1, C):
+            p = 1
+            for j in range(C):
+                p = (p - 1) * K + min(i + j, K)
+            tiles.append(p)
+
+        print(f'Case #{case}:', *tiles)
 
 
 main()
