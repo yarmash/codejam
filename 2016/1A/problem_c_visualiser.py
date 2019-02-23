4c1229369f84492d77e708088f0ff1a8288fd7e4
Eugene Yarmash <e.yarmash@gmail.com>
2019-02-23 16:42:04 +0300

    Add a script to visualize the graphs using Neo4j
    Also, add configuration files to start Neo4j in a Docker container.

---
+++ 2016/1A/problem_c_visualiser.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+
+from py2neo import Graph
+from py2neo.data import Node, Relationship
+
+
+def main():
+
+    # pass the input file as an argument instead of stdin to allow for using input()
+    with open(sys.argv[1]) as f:
+        T = int(next(f))  # the number of test cases
+        cases = [(int(next(f)), map(int, next(f).split())) for case in range(T)]
+
+    graph = Graph()
+
+    for case, (N, kids) in enumerate(cases, 1):
+        print(f'Test case #{case}, N={N}', end=' ')
+
+        graph.run('MATCH (n) DETACH DELETE n')
+
+        nodes = [None, *[Node('Kid', name=i) for i in range(1, N+1)]]
+
+        for i, v in enumerate(kids, 1):
+            rel = Relationship(nodes[i], 'BFF', nodes[v])
+            graph.create(rel)
+
+        input('(press Enter to continue) ')
+
+
+main()
