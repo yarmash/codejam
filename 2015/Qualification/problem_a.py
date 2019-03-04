d65197d7e7edcfa6a39cb6b3569351ba9eb85eb2
Eugene Yarmash <e.yarmash@gmail.com>
2019-03-04 16:10:20 +0300

    Add a solution to the problem A (Standing Ovation)

---
+++ 2015/Qualification/problem_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Standing Ovation"""
+
+
+def main():
+    T = int(input())  # the number of test cases
+
+    for case in range(1, T+1):
+        smax, s = input().split()
+        audience = to_add = 0
+
+        for shyness, num_people in enumerate(map(int, s)):
+            if num_people:
+                if shyness <= audience:
+                    audience += num_people
+                else:
+                    to_add += shyness - audience
+                    audience += (shyness - audience) + num_people
+
+        print(f'Case #{case}: {to_add}')
+
+
+main()
