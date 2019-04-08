94e0ed261795d71750aab18b64e2e5e2797b56cc
Eugene Yarmash <e.yarmash@gmail.com>
2019-04-08 16:07:04 +0300

    Minor optimization

--- 2018/Practice/number_guessing.py
+++ 2018/Practice/number_guessing.py
@@ -6,13 +6,13 @@
 def main():
     T = int(input())  # the number of test cases
 
-    for case in range(1, T+1):
+    for case in range(T):
         A, B = map(int, input().split())  # exclusive lower bound and inclusive upper bound
         N = int(input())  # the maximum number of guesses
 
         A += 1
 
-        while True:
+        while A <= B:
             guess = (A + B) // 2
             print(guess, flush=True)
             res = input()
