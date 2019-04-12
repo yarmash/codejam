62b461b2184d8aaa2920bb17e7a5cc7bc489fdf9
Eugene Yarmash <e.yarmash@gmail.com>
2019-04-12 17:20:25 +0300

    Add a solution for Test set 1

--- 2019/Qualification/dat_bae.py
+++ 2019/Qualification/dat_bae.py
@@ -2,53 +2,28 @@
 
 """Dat Bae"""
 
-from itertools import repeat
-import sys
-
 
 def main():
-
     T = int(input())  # the number of test cases
 
     for i in range(T):
         N, B, F = map(int, input().split())  # the number of workers / broken workers / calls
-        workers = [True]*N
 
-        def p(x):
-            test = [0]*(x+1) + [1]*(N-x-1)
-            assert len(test) == N
-            print(*test, sep='', flush=True)
+        columns = ['{:0{width}b}'.format(n, width=F) for n in range(N)]
+        result = ['']*(N-B)
+
+        for i in range(F):
+            print(*(c[i] for c in columns), sep='', flush=True)
             resp = input()
             if resp == '-1':
-                sys.exit()
-            return resp.count('0') + b == x+1
+                return
+            for i, v in enumerate(resp):
+                result[i] += v
 
-        b = left = 0
-
-        for _ in repeat(None, F):
-            # use binary search to find the rightmost index for which there're no broken workers
-            right = N-1
-            while left < right:
-                middle = (right + left) // 2
-
-                if p(middle):
-                    left = middle + 1
-                else:
-                    right = middle
-
-            workers[left] = False
-
-            b += 1
-            left += 1
-
-            if b == B:
-                print(*(i for i, v in enumerate(workers) if not v), flush=True)
-                resp = input()
-                if resp == '1':
-                    break
-                if resp == '-1':
-                    return
-        else:
+        received_columns = set(result)
+        print(*(i for i, c in enumerate(columns) if c not in received_columns), flush=True)
+        resp = input()
+        if resp == '-1':
             return
 
 
