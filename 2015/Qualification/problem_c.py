f7feee0f9043ace4722d503c2b143fd6b07dd95f
Eugene Yarmash <e.yarmash@gmail.com>
2019-10-03 13:18:47 +0300

    Add a (brute force) solution to the 2015's Dijkstra problem

---
+++ 2015/Qualification/problem_c.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+"""Dijkstra"""
+
+
+mul_map = {
+    1: {
+        'i': 'i',
+        'j': 'j',
+        'k': 'k',
+    },
+    -1: {
+        'i': '-i',
+        'j': '-j',
+        'k': '-k',
+    },
+    'i': {
+        'i': -1,
+        'j': 'k',
+        'k': '-j',
+    },
+    '-i': {
+        'i': 1,
+        'j': '-k',
+        'k': 'j',
+    },
+    'j': {
+        'i': '-k',
+        'j': -1,
+        'k': 'i',
+    },
+    '-j': {
+        'i': 'k',
+        'j': 1,
+        'k': '-i',
+    },
+    'k': {
+        'i': 'j',
+        'j': '-i',
+        'k': -1,
+    },
+    '-k': {
+        'i': '-j',
+        'j': 'i',
+        'k': 1,
+    },
+}
+
+
+def check_string(S):
+    x = 1
+    for i in range(len(S) - 2):
+        x = mul_map[x][S[i]]
+
+        if x == 'i':
+            y = 1
+            for j in range(i+1, len(S) - 1):
+                y = mul_map[y][S[j]]
+                if y == 'j':
+                    z = 1
+                    for k in range(j+1, len(S)):
+                        z = mul_map[z][S[k]]
+                    if z == 'k':
+                        return True
+    return False
+
+
+def main():
+    T = int(input())  # the number of test cases
+
+    for case in range(1, T+1):
+        L, X = map(int, input().split())
+        C = input()
+
+        if L == 1 or len(set(C)) == 1:
+            print(f'Case #{case}: NO')
+            continue
+
+        S = C*X
+
+        if check_string(S):
+            print(f'Case #{case}: YES')
+        else:
+            print(f'Case #{case}: NO')
+
+
+main()
