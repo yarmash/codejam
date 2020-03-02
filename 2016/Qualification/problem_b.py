1a6dbf4b6b098411cb1c3436ec1574f34f850af9
Eugene Yarmash <e.yarmash@gmail.com>
2020-03-02 18:27:20 +0300

    Get rid of str.endswith()

--- 2016/Qualification/problem_b.py
+++ 2016/Qualification/problem_b.py
@@ -9,7 +9,7 @@ def main():
     for case in range(1, T+1):
         S = input()  # the stack
         grouped_height = 1 + S.count('-+') + S.count('+-')
-        flips = grouped_height if S.endswith('-') else grouped_height - 1
+        flips = grouped_height if S[-1] == '-' else grouped_height - 1
         print(f'Case #{case}: {flips}')
 
 
