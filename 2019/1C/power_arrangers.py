4e3d84f821a599a8ffc332bb907e22b02d8aa274
Eugene Yarmash <e.yarmash@gmail.com>
2020-03-08 15:07:36 +0300

    Optimize the solution for conciseness

--- 2019/1C/power_arrangers.py
+++ 2019/1C/power_arrangers.py
@@ -2,76 +2,28 @@
 
 """Power Arrangers"""
 
-from collections import Counter
+from collections import defaultdict
 
 
 def main():
     T, F = map(int, input().split())  # the number of test cases and the number of figures allowed to inspect
 
     for _ in range(T):
-        figures = {1: {}, 2: {}, 3: {}, 4: {}}
+        sets = [i*5 for i in range(119)]  # starting position of each set
         missing = []
-
-        # find out the 1st letter
-        for i in range(1, 596, 5):
-            print(i, flush=True)
-            letter = input()
-            if letter == 'N':
-                return
-            figures[1][i] = letter
-
-        c = Counter(figures[1].values())
-
-        for k, v in c.items():
-            if v == 23:
-                missing.append(k)
-                break
-
-        # find out the 2nd letter
-        for k, v in figures[1].items():
-            if v == missing[-1]:
-                print(k+1, flush=True)
-                letter = input()
-                if letter == 'N':
+        for ordinal in (1, 2, 3, 5):  # number of a figure in a set
+            letters = defaultdict(list)
+            for s in sets:
+                print(s+ordinal, flush=True)
+                result = input()
+                if result == 'N':
                     return
-                figures[2][k+1] = letter
+                letters[result].append(s)
 
-        c = Counter(figures[2].values())
+            letter, sets = min(letters.items(), key=lambda item: len(item[1]))
+            missing.append(letter)
 
-        for k, v in c.items():
-            if v == 5:
-                missing.append(k)
-                break
-
-        # find out the 3rd letter
-        for k, v in figures[2].items():
-            if v == missing[-1]:
-                print(k+1, flush=True)
-                letter = input()
-                if letter == 'N':
-                    return
-                figures[3][k+1] = letter
-
-        c = Counter(figures[3].values())
-
-        for k, v in c.items():
-            if v == 1:
-                missing.append(k)
-                break
-
-        # find out the 4th letter
-        for k, v in figures[3].items():
-            if v == missing[-1]:
-                print(k+1, flush=True)
-                letter = input()
-                if letter == 'N':
-                    return
-                figures[4][k+1] = letter
-
-        missing.extend(set('ABCDE') - set(missing) - set(figures[4].values()))
-
-        # find out the 5th letter
-        missing.extend(set('ABCDE') - set(missing))
+        missing += set('ABCDE') - set(missing)
 
         print(''.join(missing))
         result = input()
