4597e3fa34cdfc64a6877bea38552179110c07ff
Eugene Yarmash <e.yarmash@gmail.com>
2021-10-27 15:41:06 +0300

    Simplify the solution
    Basically, it's the same greedy algorithm

--- 2021/Qualification/moons_and_umbrellas.py
+++ 2021/Qualification/moons_and_umbrellas.py
@@ -2,23 +2,6 @@
 
 """Moons and Umbrellas"""
 
-from collections import Counter
-
-
-def choose(pic, S, i, X, Y):
-    cost = 0
-    if pic == 'J':
-        if i > 0 and S[i-1] == 'C':
-            cost += X
-        if i < len(S)-1 and S[i+1] == 'C':
-            cost += Y
-    else:
-        if i > 0 and S[i-1] == 'J':
-            cost += Y
-        if i < len(S)-1 and S[i+1] == 'J':
-            cost += X
-    return cost
-
 
 def main():
     T = int(input())
@@ -27,41 +10,22 @@ def main():
         X, Y, S = input().split()
         X = int(X)
         Y = int(Y)
-        print(X, Y, S)
-
         S = list(S)
 
         if S[0] == '?':
             for i in range(1, len(S)):
                 if S[i] != '?':
+                    S[:i] = [S[i]]*i
                     break
+            else:
+                S[:] = ['J']*len(S)
 
-            for j in range(i-1, -1, -1):
-                cost1 = choose('J', S, j, X, Y)
-                cost2 = choose('C', S, j, X, Y)
-                print(j, cost1, cost2, S)
-
-                if cost1 < cost2:
-                    S[j] = 'J'
-                elif cost2 < cost1:
-                    S[j] = 'C'
-                else:
-                    S[j] = 'J'
-        print(S)
-
-        for i in range(len(S)):
+        for i in range(1, len(S)):
             if S[i] == '?':
-                cost1 = choose('J', S, i, X, Y)
-                cost2 = choose('C', S, i, X, Y)
-                if cost1 < cost2:
-                    S[i] = 'J'
-                elif cost2 < cost1:
-                    S[i] = 'C'
-                else:
-                    S[i] = 'J'
+                S[i] = S[i-1]
 
         S = ''.join(S)
-        cost = X*S.count('CJ') + Y * S.count('JC')
+        cost = X * S.count('CJ') + Y * S.count('JC')
 
         print(f'Case #{case}: {cost}')
 
