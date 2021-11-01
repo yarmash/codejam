480dfb780de1a78ea5e4987d1f7a7b98f5f22c58
Eugene Yarmash <e.yarmash@gmail.com>
2021-11-01 14:08:30 +0300

    Solve using bottom-up DP approach

--- 2021/Qualification/moons_and_umbrellas.py
+++ 2021/Qualification/moons_and_umbrellas.py
@@ -2,42 +2,49 @@
 
 """Moons and Umbrellas"""
 
-import sys
-from functools import lru_cache
 
-sys.setrecursionlimit(5000)  # TODO: get rid of recursion
+INF = float('inf')
+
+
+def solve_case(case):
+    X, Y, S = input().split()
+    X = int(X)  # CJ
+    Y = int(Y)  # JC
+
+    dp = [None]*len(S)
+    dp[0] = {'C': 0, 'J': INF} if S[0] == 'C' else {'C': INF, 'J': 0} if S[0] == 'J' else {'C': 0, 'J': 0}
+
+    for i, c in enumerate(S[1:], 1):
+        if c == 'C':
+            if S[i-1] == 'C':
+                dp[i] = {'C': dp[i-1]['C'], 'J': INF}
+            elif S[i-1] == 'J':
+                dp[i] = {'C': dp[i-1]['J'] + Y, 'J': INF}
+            else:
+                dp[i] = {'C': min(dp[i-1]['C'], dp[i-1]['J'] + Y), 'J': INF}
+        elif c == 'J':
+            if S[i-1] == 'C':
+                dp[i] = {'C': INF, 'J': dp[i-1]['C'] + X}
+            elif S[i-1] == 'J':
+                dp[i] = {'C': INF, 'J': dp[i-1]['J']}
+            else:
+                dp[i] = {'C': INF, 'J': min(dp[i-1]['J'], dp[i-1]['C'] + X)}
+        else:
+            if S[i-1] == 'C':
+                dp[i] = {'C': dp[i-1]['C'], 'J': dp[i-1]['C'] + X}
+            elif S[i-1] == 'J':
+                dp[i] = {'C': dp[i-1]['J'] + Y, 'J': dp[i-1]['J']}
+            else:
+                dp[i] = {'C': min(dp[i-1]['C'], dp[i-1]['J'] + Y), 'J': min(dp[i-1]['J'], dp[i-1]['C'] + X)}
+
+    print(f'Case #{case}: {min(dp[-1].values())}')
 
 
 def main():
     T = int(input())
 
     for case in range(1, T+1):
-        X, Y, S = input().split()
-        X = int(X)
-        Y = int(Y)
-        S = tuple(S)
-
-        @lru_cache(maxsize=128)
-        def cost(S):
-            if len(S) <= 1:
-                return 0
-
-            if S[0] == '?':
-                return min(cost(('C',) + S[1:]), cost(('J',) + S[1:]))
-
-            if S[0] != '?' and S[1] == '?':
-                return min(cost((S[0],) + ('C',) + S[2:]), cost((S[0],) + ('J',) + S[2:]))
-
-            if S[0] == S[1]:
-                return cost(S[1:])
-
-            if S[0] == 'C' and S[1] == 'J':
-                return X + cost(S[1:])
-
-            if S[0] == 'J' and S[1] == 'C':
-                return Y + cost(S[1:])
-
-        print(f'Case #{case}: {cost(S)}')
+        solve_case(case)
 
 
 main()
