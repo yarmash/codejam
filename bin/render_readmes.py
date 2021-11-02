474983cf8fd035c0caab50ef06855b214aaeeb1c
Eugene Yarmash <e.yarmash@gmail.com>
2021-11-02 11:03:34 +0300

    Fix whitespace issues; skip extra years in the root README

--- bin/render_readmes.py
+++ bin/render_readmes.py
@@ -10,7 +10,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def main():
     with open('dump.json') as f:
-        years = json.load(f)
+        years = [year for year in json.load(f) if Path(year['name']).is_dir()]
 
     templates_dir = Path(__file__).resolve().parents[1] / 'templates'
 
@@ -19,6 +19,7 @@ def main():
         autoescape=select_autoescape(['md']),
         trim_blocks=True,
         lstrip_blocks=True,
+        keep_trailing_newline=True,
     )
     generate_root_readme(env, years)
     generate_year_readmes(env, years)
@@ -28,16 +29,15 @@ def generate_root_readme(env, years):
     template = env.get_template('README-root.md.jinja')
 
     with open('README.md', 'w') as f:
-        print(template.render(years=years), file=f)
+        f.write(template.render(years=years))
 
 
 def generate_year_readmes(env, years):
     template = env.get_template('README-year.md.jinja')
 
     for year in years:
-        if Path(year['name']).is_dir():
-            with open(f'{year["name"]}/README.md', 'w') as f:
-                print(template.render(year=year), file=f)
+        with open(f'{year["name"]}/README.md', 'w') as f:
+            f.write(template.render(year=year))
 
 
 if __name__ == '__main__':
