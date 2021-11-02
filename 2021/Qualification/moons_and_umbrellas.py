f4453afbe49be76d326598db568e94fd63b28198
Eugene Yarmash <e.yarmash@gmail.com>
2021-11-02 13:48:25 +0300

    Update the algorithm to use O(1) space instead of O(n)

--- 2021/Qualification/moons_and_umbrellas.py
+++ 2021/Qualification/moons_and_umbrellas.py
@@ -11,33 +11,32 @@ def solve_case(case):
     X = int(X)  # CJ
     Y = int(Y)  # JC
 
-    dp = [None]*len(S)
-    dp[0] = {'C': 0, 'J': INF} if S[0] == 'C' else {'C': INF, 'J': 0} if S[0] == 'J' else {'C': 0, 'J': 0}
+    dp = {'C': 0, 'J': INF} if S[0] == 'C' else {'C': INF, 'J': 0} if S[0] == 'J' else {'C': 0, 'J': 0}
 
     for i, c in enumerate(S[1:], 1):
         if c == 'C':
             if S[i-1] == 'C':
-                dp[i] = {'C': dp[i-1]['C'], 'J': INF}
+                dp = {'C': dp['C'], 'J': INF}
             elif S[i-1] == 'J':
-                dp[i] = {'C': dp[i-1]['J'] + Y, 'J': INF}
+                dp = {'C': dp['J'] + Y, 'J': INF}
             else:
-                dp[i] = {'C': min(dp[i-1]['C'], dp[i-1]['J'] + Y), 'J': INF}
+                dp = {'C': min(dp['C'], dp['J'] + Y), 'J': INF}
         elif c == 'J':
             if S[i-1] == 'C':
-                dp[i] = {'C': INF, 'J': dp[i-1]['C'] + X}
+                dp = {'C': INF, 'J': dp['C'] + X}
             elif S[i-1] == 'J':
-                dp[i] = {'C': INF, 'J': dp[i-1]['J']}
+                dp = {'C': INF, 'J': dp['J']}
             else:
-                dp[i] = {'C': INF, 'J': min(dp[i-1]['J'], dp[i-1]['C'] + X)}
+                dp = {'C': INF, 'J': min(dp['J'], dp['C'] + X)}
         else:
             if S[i-1] == 'C':
-                dp[i] = {'C': dp[i-1]['C'], 'J': dp[i-1]['C'] + X}
+                dp = {'C': dp['C'], 'J': dp['C'] + X}
             elif S[i-1] == 'J':
-                dp[i] = {'C': dp[i-1]['J'] + Y, 'J': dp[i-1]['J']}
+                dp = {'C': dp['J'] + Y, 'J': dp['J']}
             else:
-                dp[i] = {'C': min(dp[i-1]['C'], dp[i-1]['J'] + Y), 'J': min(dp[i-1]['J'], dp[i-1]['C'] + X)}
+                dp = {'C': min(dp['C'], dp['J'] + Y), 'J': min(dp['J'], dp['C'] + X)}
 
-    print(f'Case #{case}: {min(dp[-1].values())}')
+    print(f'Case #{case}: {min(dp.values())}')
 
 
 def main():
