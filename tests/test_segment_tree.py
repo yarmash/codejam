5e80c516ee202fe238045ed52002fcaa0a80eee0
Eugene Yarmash <e.yarmash@gmail.com>
2023-02-20 18:57:14 +0300

    Simplify the tests for SegmentTree
    Use `reduce` to apply the segment tree function cumulatively to the
    elements in the range.

--- tests/test_segment_tree.py
+++ tests/test_segment_tree.py
@@ -1,18 +1,12 @@
 """Tests for the SegmentTree class."""
 
 import random
+from functools import reduce
 
 import pytest
-
 from codejam.segment_tree import SegmentTree
 
 SEQ_LEN = 100
-
-QUERY_TYPE_TO_FUNC = {
-    'sum': sum,
-    'min': min,
-    'max': max,
-}
 
 
 @pytest.fixture
@@ -27,15 +21,13 @@ def segment_tree(random_seq, request):
 
 @pytest.mark.parametrize('left,right', [(0, 0), (0, SEQ_LEN-1), (10, SEQ_LEN-10)])
 def test_query(random_seq, segment_tree, left, right):
-    # Note: we can't use `segment_tree.func` here, because that is generally a function of two
-    # arguments, while in the test we need a function that works on a range.
-    func = QUERY_TYPE_TO_FUNC[segment_tree.query_type]
-    assert segment_tree.query(left, right) == func(random_seq[left:right+1])
+    # Note: we can't use `segment_tree.func` directly here, because that is generally
+    # a function of two arguments, while in the test we need a function that works on a range.
+    assert segment_tree.query(left, right) == reduce(segment_tree.func, random_seq[left:right+1])
 
 
-@pytest.mark.parametrize('pos,value', [(0, -10**5), (SEQ_LEN//2, 10**5)])
+@pytest.mark.parametrize('pos,value', [(0, -10**5), (SEQ_LEN//2, 10**5), (SEQ_LEN-1, -10**5)])
 def test_modify(random_seq, segment_tree, pos, value):
-    func = QUERY_TYPE_TO_FUNC[segment_tree.query_type]
     segment_tree.modify(pos, value)
     random_seq[pos] = value
-    assert segment_tree.query(0, SEQ_LEN-1) == func(random_seq[:SEQ_LEN])
+    assert segment_tree.query(0, SEQ_LEN-1) == reduce(segment_tree.func, random_seq)
